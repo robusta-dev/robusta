3a6f85392cb82615a41e166335649815f50b3a65
Natan Yellin <aantn@users.noreply.github.com>
2021-06-28 15:13:53 +0300

    minor improvements to crash loop reporter

--- playbooks/restart_loop_reporter.py
+++ playbooks/restart_loop_reporter.py
@@ -27,7 +27,13 @@ def restart_loop_reporter(event: PodEvent, config: RestartLoopParams):
         return
 
     for container_status in crashed_container_statuses:
-        event.report_blocks.append(MarkdownBlock(f'*{container_status.name} restart* count: {container_status.restartCount}'))
+        event.report_blocks.append(MarkdownBlock(f'*{container_status.name}* restart count: {container_status.restartCount}'))
+        if container_status.state and container_status.state.waiting:
+            event.report_blocks.append(MarkdownBlock(f'*{container_status.name}* waiting reason: {container_status.state.waiting.reason}'))
+        if container_status.state and container_status.state.terminated:
+            event.report_blocks.append(MarkdownBlock(f'*{container_status.name}* termination reason: {container_status.state.terminated.reason}'))
+        if container_status.lastState and container_status.lastState.terminated:
+            event.report_blocks.append(MarkdownBlock(f'*{container_status.name}* termination reason: {container_status.lastState.terminated.reason}'))
         container_log = pod.get_logs(container_status.name, previous=True)
         if container_log:
             event.report_blocks.append(FileBlock(f"{pod_name}.txt", container_log))
@@ -35,7 +41,7 @@ def restart_loop_reporter(event: PodEvent, config: RestartLoopParams):
             event.report_blocks.append(MarkdownBlock(f"Container logs unavailable for container: {container_status.name}"))
             logging.error(f"could not fetch logs from container: {container_status.name}. logs were {container_log}")
 
-        event.report_title = f"Crashing pod {pod.metadata.name} in namespace {pod.metadata.namespace} ({container_status.state.waiting.reason})"
-        event.slack_channel = config.slack_channel
-        send_to_slack(event)
+    event.report_title = f"Crashing pod {pod.metadata.name} in namespace {pod.metadata.namespace}"
+    event.slack_channel = config.slack_channel
+    send_to_slack(event)
 
