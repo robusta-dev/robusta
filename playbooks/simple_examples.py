4f0dcc528fb0f14bafcd95d8e61697142cd93a4f
Natan Yellin <aantn@users.noreply.github.com>
2021-06-28 17:21:24 +0300

    move example playbooks to a dedicated file

--- playbooks/simple_examples.py
+++ playbooks/simple_examples.py
@@ -35,3 +35,19 @@ def slack_confirmation_on_cpu(event: PrometheusKubernetesAlert, config: HighCpuC
     ])
 
     send_to_slack(event)
+
+
+@on_pod_create
+def test_pod_orm(event: PodEvent):
+    logging.info('running test_pod_orm')
+    pod = event.obj
+
+    images = [container.image for container in event.obj.spec.containers]
+    logging.info(f'pod images are {images}')
+
+    exec_resp = pod.exec("ls -l /")
+    logging.info(f'pod ls / command: {exec_resp}')
+
+    logging.info(f'deleting pod {pod.metadata.name}')
+    RobustaPod.deleteNamespacedPod(pod.metadata.name, pod.metadata.namespace)
+    logging.info(f'pod deleted')
