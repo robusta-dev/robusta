c189d0d72a1239cbcc0270f8c49d4d0d57e4ca0f
Natan Yellin <aantny@gmail.com>
2021-07-11 13:28:22 +0300

    only import cairosvg when needed

--- src/robusta/core/reporting/utils.py
+++ src/robusta/core/reporting/utils.py
@@ -1,10 +1,13 @@
 from typing import List
 
 from .blocks import *
-import cairosvg  # type: ignore
 
 
 def add_pngs_for_all_svgs(blocks: List[FileBlock]):
+    # we import cairosvg here and not globally because in some environments it isn't trivially installed (e.g. windows)
+    # and we don't want to throw an exception globally when it isn't around
+    import cairosvg
+
     new_blocks = blocks.copy()
     for b in blocks:
         if not isinstance(b, FileBlock):
