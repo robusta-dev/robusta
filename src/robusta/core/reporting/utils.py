6cc15fc17f18d3038437f138951f9f0de4ebca86
Natan Yellin <aantny@gmail.com>
2021-07-11 16:27:34 +0300

    defer import cairosvg until actually necessary

--- src/robusta/core/reporting/utils.py
+++ src/robusta/core/reporting/utils.py
@@ -3,17 +3,21 @@ from typing import List
 from .blocks import *
 
 
-def add_pngs_for_all_svgs(blocks: List[FileBlock]):
+def convert_svg_to_png(svg: bytes) -> bytes:
     # we import cairosvg here and not globally because in some environments it isn't trivially installed (e.g. windows)
     # and we don't want to throw an exception globally when it isn't around
     import cairosvg
 
+    return cairosvg.svg2png(bytestring=svg)
+
+
+def add_pngs_for_all_svgs(blocks: List[FileBlock]):
     new_blocks = blocks.copy()
     for b in blocks:
         if not isinstance(b, FileBlock):
             continue
         if not b.filename.endswith(".svg"):
             continue
-        conversion = cairosvg.svg2png(bytestring=b.contents)
+        conversion = convert_svg_to_png(b.contents)
         new_blocks.append(FileBlock(b.filename.replace(".svg", ".png"), conversion))
     return new_blocks
