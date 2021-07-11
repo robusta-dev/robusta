7557f818e2639b243d487be49a1b945282a77211
Natan Yellin <aantny@gmail.com>
2021-07-11 12:23:17 +0300

    fix __all__ export

--- src/robusta/core/reporting/__init__.py
+++ src/robusta/core/reporting/__init__.py
@@ -2,15 +2,15 @@ from .blocks import *
 from .callbacks import ReportCallbackEvent, on_report_callback
 
 __all__ = [
-    BaseBlock,
-    MarkdownBlock,
-    DividerBlock,
-    FileBlock,
-    HeaderBlock,
-    ListBlock,
-    TableBlock,
-    KubernetesFieldsBlock,
-    CallbackBlock,
-    ReportCallbackEvent,
-    on_report_callback,
+    'BaseBlock',
+    'MarkdownBlock',
+    'DividerBlock',
+    'FileBlock',
+    'HeaderBlock',
+    'ListBlock',
+    'TableBlock',
+    'KubernetesFieldsBlock',
+    'CallbackBlock',
+    'ReportCallbackEvent',
+    'on_report_callback',
 ]
