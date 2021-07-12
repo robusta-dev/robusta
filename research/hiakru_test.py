f1569894671dc7a451009c0f5666f8b41c41ca47
Natan Yellin <aantny@gmail.com>
2021-07-12 12:07:43 +0300

    only run tests from the tests directory and remove old research file

--- research/hiakru_test.py
+++
@@ -1,8 +0,0 @@
-from hikaru.model import *
-from kubernetes import config
-
-config.load_kube_config()
-try:
-    x= ConfigMap.readNamespacedConfigMap("doesnt-exist", "robusta").obj
-except Exception as e:
-    pass
