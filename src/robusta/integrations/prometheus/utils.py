0f8e1a5f999e25af333fa0f83bcb454ca85739ae
arik <alon.arik@gmail.com>
2021-08-16 21:31:24 +0300

    Change to class method Fix test

--- src/robusta/integrations/prometheus/utils.py
+++ src/robusta/integrations/prometheus/utils.py
@@ -5,18 +5,18 @@ from ...utils.service_discovery import find_service_url
 
 class PrometheusDiscovery:
     prometheus_url = None
-    @staticmethod
-    def find_prometheus_url():
+    @classmethod
+    def find_prometheus_url(cls):
         """
         Try to autodiscover the url of an in-cluster grafana service
         """
-        if PrometheusDiscovery.prometheus_url:
-            return PrometheusDiscovery.prometheus_url
+        if cls.prometheus_url:
+            return cls.prometheus_url
         prometheus_selectors = ["app=kube-prometheus-stack-prometheus", "app.kubernetes.io/name=prometheus"]
         for label_selector in prometheus_selectors:
             service_url = find_service_url(label_selector)
             if service_url:
-                PrometheusDiscovery.prometheus_url = service_url
+                cls.prometheus_url = service_url
                 return service_url
         logging.error("Prometheus url could not be found. Add 'prometheus_url' under global_config")
         return None
