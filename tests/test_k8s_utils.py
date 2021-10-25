b2c076d16c9c05a6f633033c022fc8b451224888
arik <alon.arik@gmail.com>
2021-10-25 23:00:41 +0300

    remove pytest-kind from upload file test

--- tests/test_k8s_utils.py
+++ tests/test_k8s_utils.py
@@ -4,10 +4,9 @@ from kubernetes import config
 # from pytest_kind import KindCluster
 from tests.utils.kubernetes_utils import create_sleeping_deployment
 from robusta.api import *
-from tests.utils.robusta_utils import RobustaController
 
 
-def test_upload(robusta: RobustaController):
+def test_upload():
     # config.load_kube_config(str(kind_cluster.kubeconfig_path))
     sleepypod = create_sleeping_deployment()
     time.sleep(60)
