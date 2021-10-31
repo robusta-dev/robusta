6d6f05ed491216e0a8209bcb336eafd38eaefbb6
Natan Yellin <aantn@users.noreply.github.com>
2021-10-31 22:40:03 +0200

    fix bug

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
-import sphinx_rtd_theme
 import sphinx_book_theme
 
 # -- General configuration ------------------------------------------------
