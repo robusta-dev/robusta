874edfb923b65646b3a6be7dc866d0f773595332
Natan Yellin <aantn@users.noreply.github.com>
2021-11-02 08:30:22 +0200

    Update colors and logos, remove old options

--- docs/conf.py
+++ docs/conf.py
@@ -23,13 +23,6 @@
 
 # -- General configuration ------------------------------------------------
 
-# If your documentation needs a minimal Sphinx version, state it here.
-#
-# needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autosectionlabel",
     "sphinx.ext.viewcode",
@@ -69,37 +62,24 @@ pygments_style = "manni"
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
-html_theme = "furo"
-# html_theme_path = [furo.get_pygments_stylesheet()]
 
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#
+# html_theme_path = [furo.get_pygments_stylesheet()]
+html_theme = "furo"
 html_theme_options = {
-    # 'analytics_id': 'G-G03HKXT60G',
-    "collapse_navigation": False,
-    # "navigation_depth": 0,
-    # "toc_title": "On this page",
-    # "repository_url": "https://github.com/robusta-dev/robusta/",
-    # "path_to_docs": "docs",
-    # "home_page_in_toc": True,
-    # "use_download_button": False,
-    # "use_edit_page_button": True,
-    # "use_repository_button": True,
-    # 'titles_only': False
+    "sidebar_hide_name": True,
+    "light_logo": "logo.png",
+    "dark_logo": "logo-dark.png",
     "light_css_variables": {
-        # "color-brand-primary": "#255a7e",
-        # "color-brand-primary": "black",
-        "color-brand-primary": "#e83e8c",
-        "color-brand-content": "#e83e8c",
+        "color-brand-primary": "#7253ed",
+        "color-brand-content": "#7253ed",
     },
     "dark_css_variables": {
-        "color-brand-primary": "#7C4DFF",
-        "color-brand-content": "#7C4DFF",
-        "color-sidebar-link-text": "black",
+        # "color-brand-primary": "#7C4DFF",
+        # "color-brand-content": "#7C4DFF",
+        # "color-sidebar-link-text": "black",
     },
 }
+html_title = "your custom sidebar title"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -110,58 +90,7 @@ html_css_files = [
     "custom.css",
 ]
 
-# html_logo = "images/small-robusta-logo.jpg"
-# -- Options for HTMLHelp output ------------------------------------------
-
-# Output file base name for HTML help builder.
-htmlhelp_basename = "Robustadoc"
-
-# -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-    # The paper size ('letterpaper' or 'a4paper').
-    #
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    #
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    #
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    #
-    # 'figure_align': 'htbp',
-}
-
-# Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title,
-#  author, documentclass [howto, manual, or own class]).
-latex_documents = [
-    (master_doc, "Robusta.tex", "Robusta Documentation", "Robusta", "manual"),
-]
-
-# -- Options for manual page output ---------------------------------------
-
-# One entry per manual page. List of tuples
-# (source start file, name, description, authors, manual section).
-man_pages = [(master_doc, "Robusta", "Robusta Documentation", [author], 1)]
-
-# -- Options for Texinfo output -------------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-texinfo_documents = [
-    (
-        master_doc,
-        "Robusta",
-        "Robusta Documentation",
-        author,
-        "Robusta",
-        "One line description of project.",
-        "Miscellaneous",
-    ),
-]
+# html_logo = "_static/logo.png"
 
 
 def setup(app):
