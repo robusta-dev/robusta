57ed9aa5b51ed2070d8e92bf24fe63cd7014ade5
Natan Yellin <aantny@gmail.com>
2021-11-07 17:09:04 +0200

    fix html title in docs

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ html_theme_options = {
         # "color-sidebar-link-text": "black",
     },
 }
-html_title = "your custom sidebar title"
+# html_title = ""
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
