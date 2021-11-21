2c58bcfbb4fe20989d0ec2630bebb394681c485b
Natan Yellin <aantny@gmail.com>
2021-11-21 09:29:16 +0200

    fix local tests + other test improvements
    use the new `build_and_test.sh` script to run tests locally. this will build a robusta docker image and test it.

---
+++ build_and_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "installing test dependencies"
+poetry install
+poetry run pip install git+https://github.com/astanin/python-tabulate.git@b2c26bcb70e497f674b38aa7e29de12c0123708a#egg=tabulate
+
+echo "building robusta image to test"
+TAG=$(skaffold build --quiet -o '{{(index .Builds 0).Tag }}')
+
+echo "now testing: $TAG"
+poetry run pytest -s --image="$TAG"
