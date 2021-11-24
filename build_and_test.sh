5ae0bc5cfba943d1bc0360f50238523f636d048c
Tal Gerbi <tal@robusta.dev>
2021-11-24 17:18:05 +0200

    added --extras=all, made build_and_test.sh executable

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "installing test dependencies"
-poetry install
+poetry install --extras=all
 poetry run pip install git+https://github.com/astanin/python-tabulate.git@b2c26bcb70e497f674b38aa7e29de12c0123708a#egg=tabulate
 
 echo "building robusta image to test"
