9a7b16a56de616c01bb2a3000a39da9dacdfbbe2
Natan Yellin <aantny@gmail.com>
2021-12-25 14:42:07 +0200

    Fix logging errors

--- src/robusta/core/playbooks/playbooks_event_handler_impl.py
+++ src/robusta/core/playbooks/playbooks_event_handler_impl.py
@@ -103,7 +103,7 @@ class PlaybooksEventHandlerImpl(PlaybooksEventHandler):
             return self.__error_resp(
                 f"Failed to create execution instance for"
                 f" {action_name} {action_def.from_params_parameter_class}"
-                f" {action_params} {traceback.print_exc()}"
+                f" {action_params} {traceback.format_exc()}"
             )
 
         execution_event = action_def.from_params_func(instantiation_params)
@@ -195,8 +195,7 @@ class PlaybooksEventHandlerImpl(PlaybooksEventHandler):
                     sink.write_finding(finding_copy)
                 except Exception:  # Failure to send to one sink shouldn't fail all
                     logging.error(
-                        f"Failed to publish finding to sink {sink_name}",
-                        traceback.print_exc(),
+                        f"Failed to publish finding to sink {sink_name}", exc_info=True
                     )
 
     def get_global_config(self) -> dict:
