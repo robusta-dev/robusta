2d6e682a482441c530d3210c22e95d9be7045d60
Tal Gerbi <tal@robusta.dev>
2022-01-13 17:43:02 +0200

    the OOMKilled status is not always in the lastState field of the container status, but is sometimes in the state field (as in the case of job-related pods). We therefore try to look for OOMKilled in both fields w

--- playbooks/robusta_playbooks/oom_killer.py
+++ playbooks/robusta_playbooks/oom_killer.py
@@ -4,12 +4,20 @@ from datetime import datetime, timezone
 from collections import namedtuple
 
 
-def is_oom_status(status: ContainerStatus):
+def is_last_state_in_oom_status(status: ContainerStatus):
     if not status.lastState:
         return False
     if not status.lastState.terminated:
         return False
     return status.lastState.terminated.reason == "OOMKilled"
+
+
+def is_state_in_oom_status(status: ContainerStatus):
+    if not status.state:
+        return False
+    if not status.state.terminated:
+        return False
+    return status.state.terminated.reason == "OOMKilled"
 
 
 OOMKill = namedtuple("OOMKill", ["datetime", "message"])
@@ -36,9 +44,21 @@ def oom_killer_enricher(event: NodeEvent):
     oom_kills: List[List] = []
     headers = ["time", "pod", "container", "image"]
     for pod in results.items:
-        oom_statuses = filter(is_oom_status, pod.status.containerStatuses)
-        for status in oom_statuses:
-            dt = parse_kubernetes_datetime_to_ms(status.lastState.terminated.finishedAt)
+        container_id_to_oom_killed_state = {}
+
+        oom_statuses_from_state = filter(is_state_in_oom_status, pod.status.containerStatuses)
+        for status in oom_statuses_from_state:
+            container_id_to_oom_killed_state[status.state.terminated.containerID] = (status, status.state)
+
+        oom_statuses_from_last_state = filter(is_last_state_in_oom_status, pod.status.containerStatuses)
+        for status in oom_statuses_from_last_state:
+            container_id_to_oom_killed_state[status.lastState.terminated.containerID] = (status, status.lastState)
+
+        for v in container_id_to_oom_killed_state.values():
+            status = v[0]
+            state = v[1]
+
+            dt = parse_kubernetes_datetime_to_ms(state.terminated.finishedAt)
             oom_kills.append([dt, pod.metadata.name, status.name, status.image])
 
     oom_kills.sort(key=lambda o: o[0])
