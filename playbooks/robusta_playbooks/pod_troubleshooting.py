f52a7aeb72d65a3848ae9eedbbe37210c894aaa9
avi robusta <avi@robusta.dev>
2022-01-20 18:58:09 +0200

    added stack trace for multiple identical processes

--- playbooks/robusta_playbooks/pod_troubleshooting.py
+++ playbooks/robusta_playbooks/pod_troubleshooting.py
@@ -257,6 +257,102 @@ def get_debugger_warnings(data):
     return message
 
 
+def get_relevant_processes(all_processes: List[Process], params: DebuggerParams):
+    pid_to_process = {p.pid: p for p in all_processes}
+
+    if params.pid is None:
+        return [
+            p
+            for p in pid_to_process.values()
+            if "python" in p.exe and params.process_substring in " ".join(p.cmdline)
+        ]
+
+    if params.pid not in pid_to_process:
+        return []
+
+    return [pid_to_process[params.pid]]
+
+
+def get_process_blocks(
+    processes: List[Process], pod: RobustaPod, params: DebuggerParams
+) -> List[BaseBlock]:
+    blocks = [
+        TableBlock(
+            [[p.pid, p.exe, " ".join(p.cmdline)] for p in processes],
+            ["pid", "exe", "cmdline"],
+        ),
+    ]
+    # we don't enable this by default because it is bad UX. if you press a callback buttons then nothing
+    # seems to happen for 60 seconds while the playbook runs
+    if params.interactive:
+        choices = {}
+        for proc in processes:
+            updated_params = params.copy()
+            updated_params.process_substring = ""
+            updated_params.pid = proc.pid
+            choices[f"Debug {proc.pid}"] = CallbackChoice(
+                action=python_debugger,
+                action_params=updated_params,
+                kubernetes_object=pod,
+            )
+        for proc in processes:
+            updated_params = params.copy()
+            updated_params.process_substring = ""
+            updated_params.pid = proc.pid
+            choices[f"StackTrace {proc.pid}"] = CallbackChoice(
+                action=debugger_stack_trace,
+                action_params=updated_params,
+                kubernetes_object=pod,
+            )
+        blocks.append(CallbackBlock(choices))
+        blocks.append(
+            MarkdownBlock(
+                "*After clicking a button please wait up to 120 seconds for a response*"
+            )
+        )
+    return blocks
+
+@action
+def debugger_stack_trace(event: PodEvent, params: DebuggerParams):
+    """
+    Analyze memory allocation on the specified python process, for the specified duration.
+
+    Create a finding with the memory analysis results.
+    """
+    pod = event.get_pod()
+    if not pod:
+        logging.info(f"python_debugger - pod not found for event: {event}")
+        return
+
+    all_processes = pod.get_processes()
+    relevant_processes = get_relevant_processes(all_processes, params)
+    # we have exactly one process to debug
+    pid = relevant_processes[0].pid
+    finding = Finding(
+        title=f"Stacktrace on pid {pid}:",
+        source=FindingSource.MANUAL,
+        aggregation_key="python_debugger",
+        subject=FindingSubject(
+            pod.metadata.name,
+            FindingSubjectType.TYPE_POD,
+            pod.metadata.namespace,
+        ),
+    )
+    event.add_finding(finding)
+    cmd = f"debug-toolkit stack-trace {pid}"
+    output = RobustaPod.exec_in_debugger_pod(
+            pod.metadata.name,
+            pod.spec.nodeName,
+            cmd,
+        )
+    for thread_output in output.split('Thread 0x'):
+        if thread_output:
+            finding.add_enrichment(
+                [
+                    MarkdownBlock(f"```\nThread 0x{thread_output}\n```")
+                ]
+            )
+
 @action
 def python_debugger(event: PodEvent, params: DebuggerParams):
     """
