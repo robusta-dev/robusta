6f4950f49542df09d680a07adf9413718a723d47
Natan Yellin <aantn@users.noreply.github.com>
2022-01-28 16:02:48 +0200

    Add severity to node health watcher

--- playbooks/robusta_playbooks/node_enrichments.py
+++ playbooks/robusta_playbooks/node_enrichments.py
@@ -125,6 +125,7 @@ def node_health_watcher(event: NodeChangeEvent):
         title=f"Unhealthy node {event.obj.metadata.name}",
         source=FindingSource.KUBERNETES_API_SERVER,
         aggregation_key="node_not_ready",
+        severity=FindingSeverity.MEDIUM,
     )
     event.add_finding(finding, "DEFAULT")
     event.add_enrichment([KubernetesDiffBlock([], event.old_obj, event.obj)])
