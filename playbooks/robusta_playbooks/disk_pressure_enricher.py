aece70b81699e935f194e0e547713ca187743b8a
Tal Gerbi <tal@robusta.dev>
2022-02-19 15:08:15 +0200

    reporting pod-level and container-level disk distributions

--- playbooks/robusta_playbooks/disk_pressure_enricher.py
+++ playbooks/robusta_playbooks/disk_pressure_enricher.py
@@ -13,16 +13,43 @@ def disk_pressure_enricher(event: NodeEvent):
         )
         return
 
-    # Run disk-tools on node. calculate two tables from it.
+    # Run disk-tools on node and parse its json output
+    pods_distribution_str_info = RobustaPod.exec_in_debugger_pod(
+        node.metadata.name,
+        node.metadata.name,
+        "python3 /app/src/pods_distribution.py",
+        debug_image="us-central1-docker.pkg.dev/genuine-flight-317411/devel/disk-tools:1.0"
+    )
+    pods_distribution_info = json.loads(pods_distribution_str_info)
 
-    event.add_enrichment(
+    # report disk-tools warnings
+    for warning in pods_distribution_info["pods_disk_distribution"]["warnings"]:
+        logging.warning(warning)
+
+    # calculate pod-level disk distribution
+    pod_distribution_headers = ["pod_namespace", "pod_name", "disk_space"]
+    pod_distribution_rows = [
+        [
+            pod["pod_namespace"],
+            pod["pod_name"],
+            sum([container["disk_size"] for container in pod["containers"]])
+        ] for pod in pods_distribution_info["pods_disk_distribution"]["pods_distribution"]
+    ]
+    pod_distribution_rows.sort(key= lambda row: row[2], reverse = True)
+    event.add_enrichment([TableBlock(headers=pod_distribution_headers, rows=pod_distribution_rows)])
+
+    # calculate container-level disk distribution
+    container_distribution_headers = ["pod_namespace", "pod_name", "disk_space"]
+    container_distribution_rows = []
+    for pod in pods_distribution_info["pods_disk_distribution"]["pods_distribution"]:
+        container_distribution_rows.extend(
             [
-                TableBlock(
-                    headers=["a", "b", "c"],
-                    rows=[
-                        ["a1", "b1", "c1"],
-                        ["a2", "b2", "c2"]
-                    ]
-                )
+                [
+                    pod["pod_namespace"],
+                    pod["pod_name"],
+                    container["disk_size"]
+                ] for container in pod["containers"]
             ]
         )
+    pod_distribution_rows.sort(key=lambda row: (row[0], row[1], -row[2]))
+    event.add_enrichment([TableBlock(headers=container_distribution_headers, rows=container_distribution_rows)])
