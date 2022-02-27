db904b988483f7e8fc4c860639bdd4359caf9bf0
avi robusta <avi@robusta.dev>
2022-02-27 10:41:14 +0200

    getting warning event object history

--- playbooks/robusta_playbooks/events_enrichments.py
+++ playbooks/robusta_playbooks/events_enrichments.py
@@ -1,4 +1,7 @@
 from robusta.api import *
+from hikaru import get_json
+import json
+from robusta.core.playbooks.common import get_events_list, get_object_events_history
 
 
 class EventErrorReportParams(FindingKeyParams, RateLimitParams):
@@ -44,8 +47,9 @@ def event_resource_events(event: EventChangeEvent, action_params: FindingKeyPara
     if events_table:
         event.add_enrichment([events_table])
 
+
 @action
-def get_event_history(event: EventEvent):
+def get_event_history(event: ExecutionBaseEvent, params: ActionParams):
     """
 
     get_resource_events_table
@@ -53,10 +57,35 @@ def get_event_history(event: EventEvent):
     query dal for finding
 
     """
-    finding = Finding(
-        title=f"get_event_history",
-        description="Robusta ran for the first time, getting event history",
-        finding_type=FindingType.REPORT,
-        aggregation_key=f"get_event_history"
+    warning_events = get_events_list(event_type="Warning")
+    for warning_event in warning_events.items:
+        event_obj_history = get_object_events_history(warning_event.involvedObject.kind,
+                                                      warning_event.involvedObject.name,
+                                                      warning_event.involvedObject.namespace)
+        for hist_event in event_obj_history.items:
+            #event.add_finding(create_event_finding(event))
+            logging.info(
+                f"{json.dumps(get_json(hist_event), indent=4, sort_keys=True)}"
+            )
+
+def create_event_finding(event: Event):
+    """
+    Create finding based on the kubernetes event
+    """
+    k8s_obj = event.involvedObject
+
+    return Finding(
+        title=f"{event.reason} {event.type} for {k8s_obj.kind} {k8s_obj.namespace}/{k8s_obj.name}",
+        description=event.message,
+        source=FindingSource.KUBERNETES_API_SERVER,
+        severity=FindingSeverity.INFO
+        if event.type == "Normal"
+        else FindingSeverity.HIGH,
+        finding_type=FindingType.ISSUE,
+        aggregation_key=f"Kubernetes {event.type} Event",
+        subject=FindingSubject(
+            k8s_obj.name,
+            FindingSubjectType.from_kind(k8s_obj.kind),
+            k8s_obj.namespace,
         ),
-    event.add_finding(finding)
+    )
