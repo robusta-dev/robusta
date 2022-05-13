ceff7e27afdcfe965e60b38dc04bb2370d691920
Nikhil Maheshwari <nikhil.maheshwari@whitehatjr.com>
2022-05-14 01:42:28 +0530

    added documentation for custom deployment restart playbook

---
+++ docs/catalog/actions/deployment-restart.rst
@@ -0,0 +1,8 @@
+Restart Based on prometheus alert
+########################
+
+These actions are useful for automatic restart of deployment based on certain prometheus alert.
+
+Deployment Restart
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+.. robusta-action:: playbooks.robusta_playbooks.deployment_restart.deployment_restart
--- playbooks/robusta_playbooks/deployment_restart.py
+++ playbooks/robusta_playbooks/deployment_restart.py
@@ -4,12 +4,19 @@ import logging
 
 
 class DeploymentParams(RateLimitParams):
+    """
+    :var name: name of deployment that needs to restart.
+    :var namespace: namespace of deployment that needs restart.
+    """
     name: str
     namespace: str
 
 
 @action
 def deployment_restart(alert: PrometheusKubernetesAlert, params: DeploymentParams):
+    """
+    Restart given deployment based on prometheus alert condition with defined rate limiting.
+    """
     if not RateLimiter.mark_and_test(
             "deployment_restart", params.name + params.namespace, params.rate_limit
     ):
