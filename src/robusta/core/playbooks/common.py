37558ddd798a9636f7e4f64bca717d71c4f2d4b0
Natan Yellin <aantn@users.noreply.github.com>
2022-06-14 03:45:17 +0300

    refactor and improve event enrichments + improve default playbooks

--- src/robusta/core/playbooks/common.py
+++ src/robusta/core/playbooks/common.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional, List
 from hikaru.model import EventList, Event
 
 from ..reporting import TableBlock
@@ -6,29 +6,69 @@ from ..reporting.custom_rendering import RendererType
 from ...integrations.kubernetes.api_client_utils import parse_kubernetes_datetime_to_ms
 
 
-def get_resource_events_table(table_name: str, kind: str, name: str, namespace: str = None) -> Optional[TableBlock]:
-    field_selector = f"involvedObject.kind={kind},involvedObject.name={name}"
+def filter_event(
+    ev: Event, name_substring_filter: str, included_types: Optional[List[str]]
+) -> bool:
+    if name_substring_filter not in ev.involvedObject.name:
+        return False
+    if included_types is not None and ev.type.lower() not in [
+        t.lower() for t in included_types
+    ]:
+        return False
+    return True
+
+
+def get_resource_events_table(
+    table_name: str,
+    kind: str,
+    name: str = None,
+    namespace: str = None,
+    name_substring: str = "",
+    included_types: Optional[List[str]] = None,
+    max_events: Optional[int] = None,
+) -> Optional[TableBlock]:
+    field_selector = f"involvedObject.kind={kind}"
+    if name:
+        field_selector += f",involvedObject.name={name}"
     if namespace:
         field_selector += f",involvedObject.namespace={namespace}"
 
-    event_list: EventList = Event.listEventForAllNamespaces(field_selector=field_selector).obj
+    event_list: EventList = Event.listEventForAllNamespaces(
+        field_selector=field_selector
+    ).obj
+    if not event_list.items:
+        return
 
-    if event_list.items:
-        headers = ["reason", "type", "time", "message"]
-        rows = [
-            [event.reason,
-             event.type,
-             parse_kubernetes_datetime_to_ms(get_event_timestamp(event)) if get_event_timestamp(event) else 0,
-             event.message]
-            for event in event_list.items
+    headers = ["reason", "type", "time", "message"]
+    filtered_events = [
+        ev
+        for ev in event_list.items
+        if filter_event(ev, name_substring, included_types)
+    ]
+    if not filtered_events:
+        return
+
+    sorted_events = sorted(filtered_events, key=get_event_timestamp, reverse=True)
+    if max_events is not None:
+        sorted_events = sorted_events[:max_events]
+
+    rows = [
+        [
+            event.reason,
+            event.type,
+            parse_kubernetes_datetime_to_ms(get_event_timestamp(event))
+            if get_event_timestamp(event)
+            else 0,
+            event.message,
         ]
-        return TableBlock(
-                rows=rows,
-                headers=headers,
-                column_renderers={"time": RendererType.DATETIME},
-                table_name=table_name
-            )
-    return None
+        for event in sorted_events
+    ]
+    return TableBlock(
+        rows=rows,
+        headers=headers,
+        column_renderers={"time": RendererType.DATETIME},
+        table_name=table_name,
+    )
 
 
 def get_event_timestamp(event: Event):
@@ -51,4 +91,3 @@ def get_events_list(event_type: str = None) -> EventList:
         field_selector = f"type={event_type}"
         return Event.listEventForAllNamespaces(field_selector=field_selector).obj
     return Event.listEventForAllNamespaces().obj
-
