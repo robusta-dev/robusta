8f1e87fb4bb6a8cf047ccacbc2b28b612e28fb39
Avi-Robusta <97387909+Avi-Robusta@users.noreply.github.com>
2022-07-18 14:16:19 +0300

    Oomkill trigger and enricher (#427)
    * add oomkill trigger
    
    * add pod oomkill action
    
    * added ability to add horizontal limits to graphs

--- src/robusta/core/model/base_params.py
+++ src/robusta/core/model/base_params.py
@@ -102,6 +102,13 @@ class ResourceGraphEnricherParams(PrometheusParams):
     graph_duration_minutes: int = 60
 
 
+class PodResourceGraphEnricherParams(ResourceGraphEnricherParams):
+    """
+    :var display_limits: displays on the graph the pod limit for the resource if true (only CPU and Memory are supported)
+    """
+    display_limits: bool = False
+
+
 class AlertResourceGraphEnricherParams(ResourceGraphEnricherParams):
     """
     :var item_type: one of: Pod, Node (see ResourceChartItemType)
