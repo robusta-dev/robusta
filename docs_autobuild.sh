8b1fe50df6ff1fd25cc0c111476fad89e88eed19
Pavan Gudiwada <25551553+pavangudiwada@users.noreply.github.com>
2022-09-05 16:33:56 +0530

    Sphinx autobuild and doc change

--- docs_autobuild.sh
+++ docs_autobuild.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 cd docs && make clean && cd ..
+<<<<<<< HEAD
 
 if [ "$1" == "--port" ];then
     poetry run sphinx-autobuild $@ docs docs/_build/html || printf '\n\nError running docs_autobuild. \nMake sure you are using poetry >= 1.1.15. \nCheck out https://docs.robusta.dev/master/developer-guide/platform/docs-contributions.html\n'
 else
     poetry run sphinx-autobuild docs docs/_build/html || printf '\n\nError running docs_autobuild. \nMake sure you are using poetry >= 1.1.15. \nCheck out https://docs.robusta.dev/master/developer-guide/platform/docs-contributions.html\n'
 fi
+=======
+poetry run sphinx-autobuild $@ docs docs/_build/html || printf '\n\nError running docs_autobuild. \nMake sure you are using poetry >= 1.1.15. \nCheck out https://docs.robusta.dev/master/developer-guide/platform/docs-contributions.html\n'
+>>>>>>> 74c5024 (Sphinx autobuild and doc change)
