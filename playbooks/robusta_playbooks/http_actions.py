5d88093ad159b27b734f67743cced7864fbd64b8
ali.khan <ali.khan@xgrid.co>
2022-09-08 13:46:03 +0500

    Added playbooks http_action and persistent_volume_action

---
+++ playbooks/robusta_playbooks/http_actions.py
@@ -0,0 +1,150 @@
+import json
+from robusta.api import ActionParams, action, ExecutionBaseEvent, FileBlock, Finding, Optional, FindingSource, FindingType, MarkdownBlock
+from urllib.error import URLError, HTTPError
+import requests
+
+
+class HTTP_GET(ActionParams):
+    """
+    :var url: In cluster target url.
+    :var get_response: (optional) (Default: False) Send results to sink.
+    :var params: (optional) Dictionary, list of tuples or bytes to send
+        in the query string
+    """
+
+    url: str
+    get_response: Optional[bool] = False
+    params: Optional[dict] = None
+
+
+@action
+def http_get(event: ExecutionBaseEvent, action_params: HTTP_GET):
+    """
+    Run an http GET against a url
+    """
+    function_name = "http_get"
+
+    # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
+
+    finding = Finding(
+        title=f"{action_params.url} status check",
+        source=FindingSource.MANUAL,
+        aggregation_key=function_name,
+        finding_type=FindingType.REPORT,
+        failure=False,
+    )
+
+    try:
+
+        result = requests.get(action_params.url, params=action_params.params)
+        if action_params.get_response:
+            finding.title = f"Response received from {action_params.url} "
+            finding.add_enrichment(
+                [
+                    FileBlock(f"Response: ", result.text.encode()),
+                ]
+            )
+
+    except HTTPError as e:
+        finding.title = f"{action_params.url} is un-reachable"
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*Status Code*\n```\n{e.code}\n```"
+                ),
+            ]
+        )
+    except URLError as e:
+        finding.title = f"{action_params.url} is un-reachable"
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*Reason*\n```\n{e.reason}\n```"
+                ),
+            ]
+        )
+    except Exception as e:
+        finding.title = f"{action_params.url} is un-reachable"
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*Error*\n```\n{e}\n```"
+                ),
+            ]
+        )
+    event.add_finding(finding)
+
+
+class HTTP_POST(ActionParams):
+    """
+    :var url: In cluster target url.
+    :var get_response: (optional) (Default: False) Send results to sink.
+    :var data: (optional) Dictionary, list of tuples, bytes, or file-like
+        object to send in the body of request.
+    """
+
+    url: str
+    data: dict = None
+    get_response: Optional[bool] = False
+
+
+@action
+def http_post(event: ExecutionBaseEvent, action_params: HTTP_POST):
+    """
+    Run an http POST against a url
+    """
+    function_name = "http_post"
+
+    # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
+
+    finding = Finding(
+        title=f"{action_params.url} status check",
+        source=FindingSource.MANUAL,
+        aggregation_key=function_name,
+        finding_type=FindingType.REPORT,
+        failure=False,
+    )
+
+    try:
+
+        result = requests.post(
+            action_params.url, data=action_params.data)
+
+        if action_params.get_response:
+            finding.title = f"Response received from {action_params.url} "
+            finding.add_enrichment(
+                [
+
+                    FileBlock(f"Response: ", result.text.encode()),
+
+                ]
+            )
+
+    except HTTPError as e:
+        finding.title = f"{action_params.url} is un-reachable"
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*Status Code*\n```\n{e.code}\n```"
+                ),
+            ]
+        )
+    except URLError as e:
+        finding.title = f"{action_params.url} is un-reachable"
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*Reason*\n```\n{e.reason}\n```"
+                ),
+            ]
+        )
+    except Exception as e:
+        finding.title = f"{action_params.url} is un-reachable"
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*Error*\n```\n{e}\n```"
+                ),
+            ]
+        )
+    event.add_finding(finding)
---
+++ playbooks/robusta_playbooks/persistent_volume_actions.py
@@ -0,0 +1,170 @@
+import json
+from robusta.api import action, PersistentVolumeEvent, PersistentVolumeClaim, FileBlock, RobustaPod, ObjectMeta, PodSpec, Volume, PersistentVolumeClaimVolumeSource, Container, VolumeMount, MarkdownBlock, Finding, FindingSource, FindingType
+from kubernetes import client
+
+
+@action
+def volume_analysis(event: PersistentVolumeEvent):
+    """
+    This action shows you the files present on your persistent volume
+    """
+    function_name = "volume_analysis"
+    # https://docs.robusta.dev/master/developer-guide/actions/findings-api.html
+    finding = Finding(
+        title=f"Persistent Volume content",
+        source=FindingSource.MANUAL,
+        aggregation_key=function_name,
+        finding_type=FindingType.REPORT,
+        failure=False,
+    )
+
+    # Get persistent volume data the object contains data related to PV like metadata etc
+    pv = event.get_persistentvolume()
+    pv_claimref = pv.spec.claimRef
+
+    try:
+
+        if pv_claimref != None:
+            # Do this if there is a PVC attached to PV
+            pvc_obj = PersistentVolumeClaim.readNamespacedPersistentVolumeClaim(
+                name=pv_claimref.name, namespace=pv_claimref.namespace).obj
+            pod_obj = get_pod_related_to_pvc(pvc_obj, pv)
+            if (pod_obj != None):
+                # Do this if a Pod is using PVC
+
+                pod = RobustaPod.find_pod(
+                    pod_obj.metadata.name, pod_obj.metadata.namespace)
+                volume_mount_name = None
+
+                # Find name of the mounted volume on pod
+                for volume in pod.spec.volumes:
+                    if volume.persistentVolumeClaim.claimName == pv_claimref.name:
+                        volume_mount_name = volume.name
+                        break
+
+                # Use name of the mounted volume to find the correct volume mount
+                container_found_flag = False
+                container_volume_mount = None
+                for container in pod.spec.containers:
+                    if (container_found_flag):
+                        break
+                    if(container.volumeMounts):
+                        for volume_mount in container.volumeMounts:
+                            if volume_mount_name == volume_mount.name:
+                                container_volume_mount = volume_mount
+                                container_found_flag = True
+                                break
+                
+                result = pod.exec(
+                    f"ls -R {container_volume_mount.mountPath}/")
+                finding.title = f"Files present on persistent volume {pv.metadata.name} are: "
+                finding.add_enrichment(
+                    [
+
+                        FileBlock(
+                            f"Data: ", result.encode()),
+
+                    ]
+                )
+            else:
+                # Do this if no Pod is attached to PVC
+                reader_pod = persistent_volume_reader(persistent_volume=pv)
+                result = reader_pod.exec(
+                    f"ls -R {reader_pod.spec.containers[0].volumeMounts[0].mountPath}")
+                finding.title = f"Files present on persistent volume {pv.metadata.name} are: "
+                finding.add_enrichment(
+                    [
+
+                        FileBlock(
+                            f"Data: ", result.encode()),
+
+                    ]
+                )
+                # delete the reader pod
+                reader_pod.delete()
+
+        else:
+            finding.add_enrichment(
+                [
+                    MarkdownBlock(
+                        f"ERROR: Persistent volume named {pv.metadata.name} have no persistent volume claim."
+                    ),
+                ]
+            )
+
+    except RuntimeError as e:
+        finding.add_enrichment(
+            [
+                MarkdownBlock(
+                    f"*RUNTIME ERROR*\n```\n{e}\n```"
+                ),
+            ]
+        )
+    except Exception as e:
+        body = json.loads(e.body)
+        if body['code'] == 404:
+            finding.add_enrichment(
+                [
+                    MarkdownBlock(
+                        f"ERROR: {body['message']} "
+                    ),
+                ]
+            )
+        else:
+            finding.add_enrichment(
+                [
+                    MarkdownBlock(
+                        f"*ERROR*\n```\n{body}\n```"
+                    ),
+                ]
+            )
+
+    event.add_finding(finding)
+
+# returns a pod that mounts the given persistent volume
+def persistent_volume_reader(persistent_volume):
+    reader_pod_spec = RobustaPod(
+        apiVersion="v1",
+        kind="Pod",
+        metadata=ObjectMeta(
+            name="volume-inspector",
+            namespace=persistent_volume.spec.claimRef.namespace,
+        ),
+        spec=PodSpec(
+            volumes=[
+                Volume(
+                    name="pvc-mount",
+                    persistentVolumeClaim=PersistentVolumeClaimVolumeSource(
+                        claimName=persistent_volume.spec.claimRef.name
+                    )
+                )
+            ],
+            containers=[
+                Container(
+                    name="pvc-inspector",
+                    image="busybox",
+                    command=["tail"],
+                    args=["-f", "/dev/null"],
+                    volumeMounts=[
+                        VolumeMount(
+                            mountPath="/pvc",
+                            name="pvc-mount",
+                        )
+                    ],
+                )
+            ]
+        )
+    )
+    reader_pod = reader_pod_spec.create()
+    return reader_pod
+
+# function to get pod data related to a pvc
+def get_pod_related_to_pvc(pvc_obj, pv_obj):
+    v1 = client.CoreV1Api()
+    pod = None
+    pod_list = v1.list_namespaced_pod(pvc_obj.metadata.namespace)
+    for pod in pod_list.items:
+        for volume in pod.spec.volumes:
+            if volume.persistent_volume_claim:
+                if (volume.persistent_volume_claim.claim_name == pv_obj.spec.claimRef.name):
+                    return pod
