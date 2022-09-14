3b59c3c6b5e820471971fb704729ec22933dad5c
avi robusta <avi@robusta.dev>
2022-09-14 12:19:32 +0300

    small changes

--- playbooks/robusta_playbooks/prometheus_enrichments.py
+++ playbooks/robusta_playbooks/prometheus_enrichments.py
@@ -16,5 +16,5 @@ def prometheus_enricher(event: ExecutionBaseEvent, params: PrometheusQueryParams
         return
     result = run_prometheus_query(prometheus_base_url=params.prometheus_url, promql_query=params.promql_query, starts_at=None, graph_duration_minutes=params.graph_duration_minutes)
     event.add_enrichment(
-        [FileBlock(f"prometeus_query.log", f"{result}".encode())],
+        [FileBlock(f"prometeus_query.txt", f"{result}".encode())],
     )
