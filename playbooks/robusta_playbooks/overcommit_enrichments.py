f4101146726a29615674dfe422bbc27bfdb7f419
Anastasiia Dudarenko <ans.dudarenko@gmail.com>
2022-09-14 14:10:47 +0300

    Alter alert text

--- playbooks/robusta_playbooks/overcommit_enrichments.py
+++ playbooks/robusta_playbooks/overcommit_enrichments.py
@@ -17,23 +17,16 @@ def cpu_overcommited_enricher(alert: PrometheusKubernetesAlert, params: Promethe
     alert.add_enrichment(
         [
             MarkdownBlock(
-                f"*Details:* Currently the total of your pods requests"
+                f"*Stats:* Currently the total of your pods requests"
                 f" is: {round(cpu_analyzer.get_total_cpu_requests(), 2)}, "
                 f"and total of allocatable CPU is: {round(cpu_analyzer.get_total_cpu_allocatable(), 2)}."
-                f" If one of your nodes fails, there will be no enough resources to schedule some of the pods."
-            ),
-            MarkdownBlock(
-                "*Alert Explanation:* This alert doesn’t meant that the Unscheduled Pod has already appeared."
-                " It appeared to indicate that it is time to expand the cluster."
-                " See the attached video if you need more information."
             ),
         ],
         annotations={SlackAnnotations.UNFURL: False}
     )
-    alert.override_finding_attributes(description="The total CPU requests on your cluster is high. \n"
-                                                  "In case of a single node failure, some of your pods"
-                                                  " might not be scheduled because of "
-                                                  "lack of CPU resources")
+    alert.override_finding_attributes(description="Your cluster is currently OK, but if a single node "
+                                                  "fails then some pods might not be scheduled."
+                                                  " Add more CPU to your cluster to increase resilience.")
 
 
 @action
@@ -46,26 +39,19 @@ def memory_overcommited_enricher(alert: PrometheusKubernetesAlert, params: Prome
 
     alert.add_external_link(ExternalLink(
         url="https://www.youtube.com/watch?v=Ei8tXaTJi98",
-        name="CPU Overcommited"
+        name="Memory Overcommited"
     ))
 
     alert.add_enrichment(
         [
             MarkdownBlock(
-                f"*Details:* Currently the total of your pods requests is: "
+                f"*Stats:* Currently the total of your pods requests is: "
                 f"{pretty_size(mem_analyzer.get_total_mem_requests())}, "
-                f"and total of allocatable memory is: {pretty_size(mem_analyzer.get_total_mem_allocatable())}. "
-                f"If one of your nodes fails, there will be no enough resources to schedule some of the pods."
-            ),
-            MarkdownBlock(
-                "*Alert Explanation:* This alert doesn’t meant that the Unscheduled Pod has already appeared."
-                " It appeared to indicate that it is time to expand the cluster."
-                " See the attached video if you need more information."
+                f"and total of allocatable memory is: {pretty_size(mem_analyzer.get_total_mem_allocatable())}."
             ),
         ],
         annotations={SlackAnnotations.UNFURL: False}
     )
-    alert.override_finding_attributes(description="The total Memory requests on your cluster is high. \n"
-                                                  "In case of a single node failure, some of your pods"
-                                                  " might not be scheduled because of "
-                                                  "lack of Memory resources")
+    alert.override_finding_attributes(description="Your cluster is currently OK, but if a single node "
+                                                  "fails then some pods might not be scheduled. Add more Memory"
+                                                  " to your cluster to increase resilience.")
