b7cb7e57344c3c40f346afb421538c184028725c
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2022-12-28 18:06:29 +0200

    Fix more pyright errors in playbooks

--- src/robusta/core/reporting/blocks.py
+++ src/robusta/core/reporting/blocks.py
@@ -5,7 +5,7 @@
 #       HeaderBlock("foo") doesn't work. Only HeaderBlock(text="foo") would be allowed by pydantic.
 import textwrap
 from copy import deepcopy
-from typing import Any, Callable, Dict, List, Optional, Sequence
+from typing import Any, Callable, Dict, List, Optional, Sequence, Union
 
 import hikaru
 from hikaru import DiffDetail, DiffType
@@ -66,7 +66,7 @@ class FileBlock(BaseBlock):
     filename: str
     contents: bytes
 
-    def __init__(self, filename: str, contents: bytes):
+    def __init__(self, filename: str, contents: Union[bytes, str]):
         """
         :param filename: the file's name
         :param contents: the file's contents
