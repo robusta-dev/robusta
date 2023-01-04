5c9fbc58243b55dfa06e23eb5d28b07e08638c4f
avi robusta <avi@robusta.dev>
2023-01-04 09:14:19 +0200

    fixed accidental indentation + removed code

--- src/robusta/core/playbooks/playbooks_event_handler_impl.py
+++ src/robusta/core/playbooks/playbooks_event_handler_impl.py
@@ -226,40 +226,40 @@ class PlaybooksEventHandlerImpl(PlaybooksEventHandler):
                     )
                     execution_event.response = self.__error_resp(msg, ErrorCodes.PARAMS_INSTANTIATION_FAILED.value)
                     continue
-
-                try:
-                    registered_action.func(execution_event, params)
-                except ActionException as e:
-                    msg = e.msg if e.msg else f"Action Exception {e.type} while processing {action.action_name} {to_safe_str(action_params)}"
-                    logging.error(msg)
-                    execution_event.response = self.__error_resp(e.type, e.code, log=False)
-                except PrometheusNotFound as e:
-                    logging.error(str(e))
-                    execution_event.add_enrichment(
-                        [
-                            MarkdownBlock(
-                                text="Robusta couldn't connect to the Prometheus client, check if the service is "
-                                     "available. If it is, please add to *globalConfig* in *generated_values.yaml* "
-                                     "the cluster *prometheus_url*. For example:\n"
-                                     "```globalConfig:\n"
-                                     "\tprometheus_url: http://prometheus-server.monitoring.svc.cluster.local:9090```"
-                            )
-                        ]
-                    )
-                except Exception:
-                    logging.error(f"Failed to execute action {action.action_name} {to_safe_str(action_params)}")
-                    execution_event.response = self.__error_resp(
-                        ErrorCodes.ACTION_UNEXPECTED_ERROR.name,
-                        ErrorCodes.ACTION_UNEXPECTED_ERROR.value,
-                        log=False
-                    )
-                    execution_event.add_enrichment(
-                        [
-                            MarkdownBlock(
-                                text=f"Oops... Error processing {action.action_name}"
-                            )
-                        ]
-                    )
+            try:
+                registered_action.func(execution_event, params) if action_with_params else registered_action.func(
+                    execution_event)
+            except ActionException as e:
+                msg = e.msg if e.msg else f"Action Exception {e.type} while processing {action.action_name} {to_safe_str(action_params)}"
+                logging.error(msg)
+                execution_event.response = self.__error_resp(e.type, e.code, log=False)
+            except PrometheusNotFound as e:
+                logging.error(str(e))
+                execution_event.add_enrichment(
+                    [
+                        MarkdownBlock(
+                            text="Robusta couldn't connect to the Prometheus client, check if the service is "
+                                 "available. If it is, please add to *globalConfig* in *generated_values.yaml* "
+                                 "the cluster *prometheus_url*. For example:\n"
+                                 "```globalConfig:\n"
+                                 "\tprometheus_url: http://prometheus-server.monitoring.svc.cluster.local:9090```"
+                        )
+                    ]
+                )
+            except Exception:
+                logging.error(f"Failed to execute action {action.action_name} {to_safe_str(action_params)}")
+                execution_event.response = self.__error_resp(
+                    ErrorCodes.ACTION_UNEXPECTED_ERROR.name,
+                    ErrorCodes.ACTION_UNEXPECTED_ERROR.value,
+                    log=False
+                )
+                execution_event.add_enrichment(
+                    [
+                        MarkdownBlock(
+                            text=f"Oops... Error processing {action.action_name}"
+                        )
+                    ]
+                )
         return execution_event.response
 
     @classmethod
