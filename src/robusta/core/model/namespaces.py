d2be893160ee994bd839fef4209d42619e48e037
Павел Жуков <33721692+LeaveMyYard@users.noreply.github.com>
2023-01-17 11:17:15 +0200

    Remove status from NamespaceInfo

--- src/robusta/core/model/namespaces.py
+++ src/robusta/core/model/namespaces.py
@@ -4,20 +4,17 @@ from kubernetes.client import V1Namespace
 
 class NamespaceInfo(BaseModel):
     name: str
-    status: str
     deleted: bool = False
 
     @classmethod
     def from_api_server(cls, namespace: V1Namespace) -> "NamespaceInfo":
         return cls(
-            name=namespace.metadata.name,
-            status=namespace.status.phase,
+            name=namespace.metadata.name
         )
 
     @classmethod
     def from_db_row(cls, namespace: dict) -> "NamespaceInfo":
         return cls(
             name=namespace["name"],
-            status=namespace["status"],
             deleted=namespace["deleted"],
         )
