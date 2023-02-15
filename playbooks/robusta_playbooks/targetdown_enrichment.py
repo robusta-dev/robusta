bffc5d50ea892809880a38811cb3aab143ad5504
avi robusta <avi@robusta.dev>
2023-02-15 14:11:20 +0200

    fixing playbook

--- playbooks/robusta_playbooks/targetdown_enrichment.py
+++ playbooks/robusta_playbooks/targetdown_enrichment.py
@@ -1,16 +1,25 @@
 import json
+from typing import Optional
 
+from hikaru.model import PodList
 from robusta.api import PrometheusKubernetesAlert, TimedPrometheusParams, action
 from robusta.integrations.kubernetes.api_client_utils import list_available_services
 
 
+def get_alert_label(alert: PrometheusKubernetesAlert, label: str) -> Optional[str]:
+    try:
+        return alert.alert.labels[label]
+    except KeyError:
+        return None
+
 @action
-def target_down_dns_enricher(alert: PrometheusKubernetesAlert, params: TimedPrometheusParams):
+def target_down_dns_enricher(alert: PrometheusKubernetesAlert):
     """
     Enrich the finding with a detailed explanation for the cause of the CoreDNS and
     Kube-DNS unreachable
     """
-    if not alert.job or alert.job not in ["coredns", "kube-dns"]:
+    job = get_alert_label(alert, 'job')
+    if not job or job not in ["coredns", "kube-dns"]:
         return
     res = list_available_services("kube-system")
     try:
@@ -21,18 +30,18 @@ def target_down_dns_enricher(alert: PrometheusKubernetesAlert, params: TimedProm
     service_found = False
     for kube_item in items:
         kube_item_name = kube_item.get("metadata", {}).get("name")
-        if kube_item_name == alert.job:
+        if kube_item_name == get_alert_label(alert, 'service'):
             service_found = True
             break
 
     if not service_found:
         alert.override_finding_attributes(
-            description=f"The Prometheus Stack expects the {alert.job} to exist, but it can't be found"
-            f" in kube-system (some cluster managers do not provide this service)"
-            f", you can disable it by changing the following values in "
-            f"prometheus stack. "
-            f"```kube-prometheus-stack:\n\tcoreDns:"
-            f"\n\t\tenabled: false\n\tkubeDns:\n\t\tenabled: true```"
+            description=f"The Prometheus Stack expects the {job} to exist, but it can't be found"
+                        f" in kube-system (some cluster managers do not provide this service)"
+                        f", you can disable it by changing the following values in "
+                        f"prometheus stack. "
+                        f"```kube-prometheus-stack:\n\tcoreDns:"
+                        f"\n\t\tenabled: false\n\tkubeDns:\n\t\tenabled: true```"
         )
     else:
         # We cannot reach out the service, so alert should be risen
