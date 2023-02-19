60b63709dd6083cad00ccb07d44e74ae7627f57a
avi robusta <avi@robusta.dev>
2023-02-19 16:42:00 +0200

    fixed dns deployment check

--- playbooks/robusta_playbooks/targetdown_enrichment.py
+++ playbooks/robusta_playbooks/targetdown_enrichment.py
@@ -1,16 +1,20 @@
 import json
 import logging
+from typing import List
 
-from hikaru.model import PodList
+from hikaru.model import DeploymentList
 from robusta.api import PrometheusKubernetesAlert, action
 from robusta.integrations.kubernetes.api_client_utils import list_available_services
 
 
-def has_dns_pods(namespace: str, job: str) -> bool:
-    pods: PodList = PodList.listNamespacedPod(
-        namespace, label_selector=f"k8s-app = {job}"
-    ).obj
-    return len(pods.items) > 1
+def has_dns_deployment(namespace: str, job: str) -> bool:
+    labels: List[str] = [f"k8s-app = {job}", f"app.kubernetes.io/name={job}", f"app={job}"]
+    for label in labels:
+        deployments: DeploymentList = \
+            DeploymentList.listNamespacedDeployment(namespace, label_selector=label).obj
+        if len(deployments.items) > 0:
+            return True
+    return False
 
 
 def auto_silence_target_down(alert: PrometheusKubernetesAlert, job: str, reason_for_silence: str):
@@ -40,12 +44,11 @@ def target_down_dns_enricher(alert: PrometheusKubernetesAlert):
         if kube_item_name == alert.get_alert_label('service'):
             service_found = True
             break
-    job_is_coredns = job == "coredns"
 
-    # wrong service is set up i.e. coredns when should be kube-dns
-    if not has_dns_pods(alert.label_namespace, job):
-        other_job = 'kube-dns' if job_is_coredns else 'coredns'
-        silence_reason = f"{ other_job} should be configured instead"
+    other_dns = 'kube-dns' if job == "coredns" else 'coredns'
+    # wrong dns is set up i.e. coredns when should be kube-dns
+    if has_dns_deployment(alert.label_namespace, other_dns):
+        silence_reason = f"{ other_dns} should be configured instead"
         auto_silence_target_down(alert, job, silence_reason)
 
     # the service does not exist
