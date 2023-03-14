0e4710dd6f47d9a1a005a5d354d0b791f53322f3
Roi Glinik <groi.tech@gmail.com>
2023-03-14 14:39:39 +0200

    use hardcoded value

--- src/robusta/core/exceptions.py
+++ src/robusta/core/exceptions.py
@@ -1,7 +1,4 @@
-from robusta.utils.error_codes import ErrorCodes
-
 class PrometheusNotFound(Exception):
     """Exception, when Prometheus cannot be reached"""
 
-    errorCode: ErrorCodes = ErrorCodes.PROMETHEUS_DISCOVERY_FAILED
     pass
