a397aba7cbd29974deea30134d2928a2368c8f58
Robusta Runner <aantny@gmail.com>
2023-04-10 15:43:10 +0300

    Update run_runner_locally.sh

--- run_runner_locally.sh
+++ run_runner_locally.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+# IMPORTANT: this script was tested with poetry 1.4.2
+# Run `poetry self update 1.4.2` or try other versions at your own risk!
+
+# Settings you might want to change
 export PYTHON_BINARY=python3.9
+export PORT=5000
+
+# Internal constants
 export RED='\033[0;31m'
 export NC='\033[0m' # No Color
-
 
 # Check if Python 3.9 or higher is installed
 if ! $PYTHON_BINARY -c "import sys; exit(not (sys.version_info.major == 3 and sys.version_info.minor >= 9))"; then
@@ -13,13 +19,6 @@ if ! $PYTHON_BINARY -c "import sys; exit(not (sys.version_info.major == 3 and sy
   echo "To change your Python version, edit the PYTHON_BINARY variable at the top of this script"
   exit 1
 fi
-
-#if ! $PYTHON_BINARY -c "import poetry" &> /dev/null; then
-#  echo -e "Error: Poetry is not installed for the Python binary you chose.\n"
-#  echo "You are using `$PYTHON_BINARY --version` located at `which $PYTHON_BINARY`"
-#  echo "To change your Python version, edit the PYTHON_BINARY variable at the top of this script"
-#  exit 1
-#fi
 
 # Check if Poetry is installed globally
 if ! command -v poetry &> /dev/null
@@ -41,9 +40,9 @@ mkdir -p deployment/playbooks/defaults
 ln -fsw $(pwd)/playbooks/robusta_playbooks/ ./deployment/playbooks/defaults
 ln -fsw $(pwd)/playbooks/pyproject.toml ./deployment/playbooks/defaults
 
-echo "Checking if runner can listen on port 5000"
-if lsof -Pi :5000 -sTCP:LISTEN -t >/dev/null ; then
-    echo -e "${RED}Error: Port 5000 is already in use. Exiting.${NC}"
+echo "Checking if runner can listen on port ${PORT}"
+if lsof -Pi :${PORT} -sTCP:LISTEN -t >/dev/null ; then
+    echo -e "${RED}Error: Port ${PORT} is already in use. Exiting.${NC}"
     exit 1
 fi
 
@@ -64,7 +63,6 @@ poetry run python3 -m pip install -e ./deployment/playbooks/defaults
 export PLAYBOOKS_CONFIG_FILE_PATH=./deployment/playbooks/active_playbooks.yaml
 export INTERNAL_PLAYBOOKS_ROOT=./src/robusta/core/playbooks/internal
 export PLAYBOOKS_ROOT=./deployment/playbooks
-export PORT=5000
 export REPO_LOCAL_BASE_DIR=./deployment/git_playbooks
 export INSTALLATION_NAMESPACE=default
 export TRACE_INCOMING_REQUESTS=true
