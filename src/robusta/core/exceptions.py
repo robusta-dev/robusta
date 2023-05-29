ed7e97c5dd59e044a87af1f836b53b5e7f8df7cf
Ganesh Rathinavel Medayil <182092+ganeshrvel@users.noreply.github.com>
2023-05-29 23:43:05 +0530

    Feature/improve discovery (#908)
    * Made the discovery async
    * Optimized the prometheus check

--- src/robusta/core/exceptions.py
+++ src/robusta/core/exceptions.py
@@ -1,11 +1,26 @@
 class PrometheusNotFound(Exception):
     """Exception, when Prometheus cannot be reached"""
 
+
+class NoPrometheusUrlFound(Exception):
+    """Exception, when Prometheus cannot be reached"""
+
+    pass
+
+
 class VictoriaMetricsNotFound(Exception):
     """Exception, when VictoriaMetrics cannot be reached"""
 
     pass
+
+
 class AlertsManagerNotFound(Exception):
+    """Exception, when Prometheus cannot be reached"""
+
+    pass
+
+
+class NoAlertManagerUrlFound(Exception):
     """Exception, when Prometheus cannot be reached"""
 
     pass
