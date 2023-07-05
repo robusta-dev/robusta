a399df1dc690e5c8eb69cd16aff5a1b709917618
avi robusta <avi@robusta.dev>
2023-07-05 15:35:16 +0300

    fix link

--- src/robusta/core/reporting/base.py
+++ src/robusta/core/reporting/base.py
@@ -224,14 +224,24 @@ class Finding(Filterable):
 
     def get_investigate_uri(self, account_id: str, cluster_name: Optional[str] = None):
         uri_path = self._map_service_to_uri()
-        params = {
-            "account": account_id,
-            "clusters": f'["{cluster_name}"]' if cluster_name else None,
-            "namespaces": f'["{self.subject.namespace}"]' if self.subject.namespace else None,
-            "kind": self.service.resource_type if self.service else None,
-            "name": self.service.name if self.service else None,
-            "names": f'["{self.aggregation_key}"]' if self.aggregation_key else None,
-        }
+        kind = self.service.resource_type if self.service else None
+        if kind and kind.lower() == "job":
+            params = {
+                "account": account_id,
+                "cluster": f'"{cluster_name}"' if cluster_name else None,
+                "namespace": f'"{self.subject.namespace}"' if self.subject.namespace else None,
+                "name": f'"{self.service.name}"' if self.service else None,
+            }
+        else:
+            params = {
+                "account": account_id,
+                "clusters": f'["{cluster_name}"]' if cluster_name else None,
+                "namespaces": f'["{self.subject.namespace}"]' if self.subject.namespace else None,
+                "kind": kind,
+                "name": self.service.name if self.service else None,
+                "names": f'["{self.aggregation_key}"]' if self.aggregation_key else None,
+            }
+
         params = {k: v for k, v in params.items() if v is not None}
         uri_path = f"{uri_path}?{urlencode(params)}"
         return f"{ROBUSTA_UI_DOMAIN}/{uri_path}"
