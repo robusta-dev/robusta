b2b36255d35ce66b5612c578ebcbf0540a789c21
avi robusta <avi@robusta.dev>
2023-07-13 15:20:08 +0300

    reverting supported change

--- src/robusta/core/sinks/robusta/dal/model_conversion.py
+++ src/robusta/core/sinks/robusta/dal/model_conversion.py
@@ -97,7 +97,7 @@ class ModelConversion:
                 structured_data.append({"type": "list", "data": block.items})
             elif isinstance(block, PrometheusBlock):
                 structured_data.append(
-                    {"type": "prometheus", "data": block.data.json(), "metadata": block.metadata, "version": 1.0}
+                    {"type": "prometheus", "data": block.data.dict(), "metadata": block.metadata, "version": 1.0}
                 )
             elif isinstance(block, TableBlock):
                 if block.table_name:
