d8ded141ee9b38a68e4f04b60226719ea73ca17d
Pavel Zhukov <33721692+LeaveMyYard@users.noreply.github.com>
2023-07-20 10:14:22 +0300

    Bump KRR to 1.4.0 (#997)

--- playbooks/robusta_playbooks/krr.py
+++ playbooks/robusta_playbooks/krr.py
@@ -28,7 +28,7 @@ from robusta.api import (
     to_kubernetes_name,
 )
 
-IMAGE: str = os.getenv("KRR_IMAGE_OVERRIDE", "us-central1-docker.pkg.dev/genuine-flight-317411/devel/krr:v1.3.2")
+IMAGE: str = os.getenv("KRR_IMAGE_OVERRIDE", "us-central1-docker.pkg.dev/genuine-flight-317411/devel/krr:v1.4.0")
 KRR_MEMORY_LIMIT: str = os.getenv("KRR_MEMORY_LIMIT", "1Gi")
 
 
