9bdea3747733e44c2f74d757e1f1c14493574f10
Avi-Robusta <97387909+Avi-Robusta@users.noreply.github.com>
2023-08-13 10:56:59 +0300

    supporting secret strings for telegram chatID (#1032)
    * supporting secret strings for telegram chatID
    
    * remove extra lines

--- src/robusta/core/sinks/telegram/telegram_sink_params.py
+++ src/robusta/core/sinks/telegram/telegram_sink_params.py
@@ -1,10 +1,12 @@
+from typing import Union
+
 from robusta.core.sinks.sink_base_params import SinkBaseParams
 from robusta.core.sinks.sink_config import SinkConfigBase
 
 
 class TelegramSinkParams(SinkBaseParams):
     bot_token: str
-    chat_id: int
+    chat_id: Union[int, str]
     thread_id: int = None
     send_files: bool = True  # Change to False, to omit file attachments
 
