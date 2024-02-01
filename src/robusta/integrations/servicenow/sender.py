ecf0f71e54600e886a2eb560c7038ccb8b2c1f34
Robert Szefler <robert@robusta.dev>
2024-02-01 21:40:27 +0100

    formatting fixes

--- src/robusta/integrations/servicenow/sender.py
+++ src/robusta/integrations/servicenow/sender.py
@@ -1,11 +1,35 @@
-from typing import Dict
+from typing import Dict, List
 import logging
 
 import requests
 import zeep
 
+from robusta.core.reporting import FindingSource
+from robusta.core.reporting.blocks import MarkdownBlock, LinkProp, LinksBlock, FileBlock
+
 from robusta.core.reporting.base import BaseBlock, Emojis, Finding, FindingStatus
+from robusta.core.reporting.consts import EnrichmentAnnotation
 from robusta.core.sinks.servicenow.servicenow_sink_params import ServiceNowSinkParams
+from robusta.core.sinks.transformer import Transformer
+
+
+class ServiceNowTransformer(Transformer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.file_blocks: List[FileBlock] = []
+
+    def block_to_html(self, block: BaseBlock) -> str:
+        if isinstance(block, FileBlock):
+            self.file_blocks.append(block)
+            return f"<p>See attachment {block.filename}</p>"
+        elif isinstance(block, LinksBlock):
+            return (
+                f"<ul>\n"
+                + "\n".join(f'  <li><a href="{link.url}">{link.text}</a></li>' for link in block.links)
+                + "\n</ul>\n"
+            )
+        else:
+            return super().block_to_html(block)
 
 
 class ServiceNowSender:
@@ -23,17 +47,38 @@ class ServiceNowSender:
             FindingStatus.RESOLVED if finding.title.startswith("[RESOLVED]") else FindingStatus.FIRING
         )
 
-        message = self.format_message(finding)
+        message = self.format_message(finding, platform_enabled)
         header = self.format_header(finding, status)
         wsdl_url = f"https://{self.params.instance}.service-now.com/incident.do?WSDL"
         client = zeep.CachingClient(wsdl_url, transport=self.transport)
         soap_payload = self.params_to_soap_payload(self.get_params_dict(header, message, self.params.caller_id))
         response = client.service.insert(**soap_payload)
-        logging.warning(f"XXXXX {response=}")
+        # TODO check response
 
-    @staticmethod
-    def format_message(finding: Finding) -> str:
-        return ""
+    def format_message(self, finding: Finding, platform_enabled: bool) -> str:
+        blocks: List[BaseBlock] = []
+
+        if platform_enabled:
+            blocks.append(self.__create_links(finding, html_class="header_links"))
+
+        blocks.append(MarkdownBlock(text=f"*Source:* `{self.cluster_name}`"))
+        if finding.description:
+            if finding.source == FindingSource.PROMETHEUS:
+                blocks.append(MarkdownBlock(f"{Emojis.Alert.value} *Alert:* {finding.description}"))
+            elif finding.source == FindingSource.KUBERNETES_API_SERVER:
+                blocks.append(
+                    MarkdownBlock(f"{Emojis.K8Notification.value} *K8s event detected:* {finding.description}")
+                )
+            else:
+                blocks.append(MarkdownBlock(f"{Emojis.K8Notification.value} *Notification:* {finding.description}"))
+
+        for enrichment in finding.enrichments:
+            if enrichment.annotations.get(EnrichmentAnnotation.SCAN, False):
+                enrichment.blocks = [Transformer.scanReportBlock_to_fileblock(b) for b in enrichment.blocks]
+            blocks.extend(enrichment.blocks)
+
+        transformer = ServiceNowTransformer()
+        return "[code]" + transformer.to_html(blocks).strip() + "[/code]"
 
     def format_header(self, finding: Finding, status: FindingStatus) -> str:
         title = finding.title.removeprefix("[RESOLVED] ")
@@ -41,6 +86,28 @@ class ServiceNowSender:
         status_name: str = "Prometheus Alert Firing" if status == FindingStatus.FIRING else "Resolved"
         status_str: str = f"{status.to_emoji()} {status_name}" if finding.add_silence_url else ""
         return f"{status_str} {sev.to_emoji()} {sev.name.upper()} {sev.to_emoji()} {title}"
+
+    def __create_links(self, finding: Finding, html_class: str):
+        links: List[LinkProp] = []
+        links.append(
+            LinkProp(
+                text="Investigate 🔎",
+                url=finding.get_investigate_uri(self.account_id, self.cluster_name),
+            )
+        )
+
+        if finding.add_silence_url:
+            links.append(
+                LinkProp(
+                    text="Configure Silences 🔕",
+                    url=finding.get_prometheus_silence_url(self.account_id, self.cluster_name),
+                )
+            )
+
+        for video_link in finding.video_links:
+            links.append(LinkProp(text=f"{video_link.name} 🎬", url=video_link.url))
+
+        return LinksBlock(links=links)
 
 # f'[code]<a href="{finding.get_investigate_uri(self.account_id, self.cluster_name)}"><b></b>[/code]'
 
