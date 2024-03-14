9b14cbf729d789247c895104f247d5df70af298b
Robert Szefler <robert@robusta.dev>
2024-03-14 17:44:06 +0100

    move check_change_filters back to K8sBaseTrigger & fix default handling

--- src/robusta/core/playbooks/base_trigger.py
+++ src/robusta/core/playbooks/base_trigger.py
@@ -4,9 +4,7 @@ from typing import Any, Dict, List, Optional, Type
 from pydantic import BaseModel
 
 from robusta.core.model.events import ExecutionBaseEvent
-from robusta.core.model.k8s_operation_type import K8sOperationType
 from robusta.core.reporting.base import Finding
-from robusta.utils.common import duplicate_without_fields, is_matching_diff
 from robusta.utils.documented_pydantic import DocumentedModel
 
 
@@ -21,29 +19,7 @@ class TriggerEvent(BaseModel):
         return "NA"
 
 
-DEFAULT_CHANGE_INCLUDE = ["spec"]
-DEFAULT_CHANGE_IGNORE = [
-    "status",
-    "metadata.generation",
-    "metadata.resourceVersion",
-    "metadata.managedFields",
-    "spec.replicas",
-]
-
-
 class BaseTrigger(DocumentedModel):
-    change_filters: Dict[str, List[str]] = None
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        if self.change_filters is None:
-            self.change_filters = {}
-        if not self.change_filters.get("include"):
-            self.change_filters["include"] = DEFAULT_CHANGE_INCLUDE
-        if not self.change_filters.get("ignore"):
-            self.change_filters["ignore"] = DEFAULT_CHANGE_IGNORE
-
     def get_trigger_event(self) -> str:
         pass
 
@@ -58,23 +34,3 @@ class BaseTrigger(DocumentedModel):
     @staticmethod
     def get_execution_event_type() -> Type[ExecutionBaseEvent]:
         pass
-
-    def check_change_filters(self, execution_event: ExecutionBaseEvent):
-        """Check if the trigger should run considering the configuration of change_filters.
-
-        Sets appropriate fields related to filtered object data on the execution_event in
-        order for them to be accessible downstream in playbooks."""
-
-        filtered_diffs = []
-        obj_filtered = duplicate_without_fields(execution_event.obj, self.change_filters["ignore"])
-        old_obj_filtered = duplicate_without_fields(execution_event.old_obj, self.change_filters["ignore"])
-
-        if execution_event.operation == K8sOperationType.UPDATE:
-            all_diffs = obj_filtered.diff(old_obj_filtered)
-            filtered_diffs = list(filter(lambda x: is_matching_diff(x, self.change_filters["include"]), all_diffs))
-            if len(filtered_diffs) == 0:
-                return False
-        execution_event.obj_filtered = obj_filtered
-        execution_event.old_obj_filtered = old_obj_filtered
-        execution_event.filtered_diffs = filtered_diffs
-        return True
