9e71ee540833225facc40806cd0b425b1f6aa9d1
avi@robusta.dev <avi@robusta.dev>
2024-06-16 11:36:04 +0300

    reverted to prevent devs from building to dockerhub, devs can still build to gcp

--- build_release.sh
+++ build_release.sh
@@ -1,11 +1,11 @@
 export DOCKER_BUILDKIT=1
 
-docker pull robustadev/robusta-runner-dev:cache
+docker us-central1-docker.pkg.dev/genuine-flight-317411/devel/robusta-runner:cache
 
 docker buildx build \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   --platform linux/arm64,linux/amd64 \
-  --cache-from robustadev/robusta-runner-dev:cache \
+  --cache-from us-central1-docker.pkg.dev/genuine-flight-317411/devel/robusta-runner:cache \
   --tag $IMAGE \
   --tag robustadev/robusta-runner:cache \
   --push \
