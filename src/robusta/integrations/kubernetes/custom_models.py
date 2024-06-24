0a67746fa488ca0369f2806f15891ac96f93de36
Arik Alon <alon.arik@gmail.com>
2024-06-24 13:54:24 +0300

    remove debug changes

--- src/robusta/integrations/kubernetes/custom_models.py
+++ src/robusta/integrations/kubernetes/custom_models.py
@@ -537,8 +537,6 @@ class RobustaJob(Job):
             pod = job.get_single_pod()
             return pod.get_logs() or ""
         finally:
-            if job and not pod:
-                pod = job.get_single_pod()
             if pod and finalizers:
                 try:  # must use patch, since the pod revision changed at this point
                     body = {"metadata": {"$deleteFromPrimitiveList/finalizers": finalizers}}
