7a66834b05c1d401783bcc2160d3cf42de407216
avi robusta <avi@robusta.dev>
2024-07-24 21:26:26 +0300

    no cache

--- build_release.sh
+++ build_release.sh
@@ -1,11 +1,8 @@
 export DOCKER_BUILDKIT=1
 
-docker pull us-central1-docker.pkg.dev/genuine-flight-317411/devel/robusta-runner:cache
-
 docker buildx build \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
   --platform linux/arm64,linux/amd64 \
-  --cache-from us-central1-docker.pkg.dev/genuine-flight-317411/devel/robusta-runner:cache \
   --tag $IMAGE \
   --tag robustadev/robusta-runner:cache \
   --push \
