cc978240c098d11b13ff1ca284536cfbc036e33a
Arik Alon <alon.arik@gmail.com>
2024-07-24 21:13:16 +0300

    build only arm

--- build_release.sh
+++ build_release.sh
@@ -4,7 +4,7 @@ docker pull us-central1-docker.pkg.dev/genuine-flight-317411/devel/robusta-runne
 
 docker buildx build \
   --build-arg BUILDKIT_INLINE_CACHE=1 \
-  --platform linux/arm64,linux/amd64 \
+  --platform linux/arm64 \
   --cache-from us-central1-docker.pkg.dev/genuine-flight-317411/devel/robusta-runner:cache \
   --tag $IMAGE \
   --tag robustadev/robusta-runner:cache \
