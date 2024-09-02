c23eb2ba0d31ec0ca3f36856b7543ae43fff131f
Roi Glinik <groi.tech@gmail.com>
2024-09-02 22:54:01 +0300

    log an error instead of marking as failed health check

--- src/robusta/core/playbooks/internal/ai_integration.py
+++ src/robusta/core/playbooks/internal/ai_integration.py
@@ -114,7 +114,7 @@ def holmes_workload_health(event: ExecutionBaseEvent, params: HolmesWorkloadHeal
             analysis = json.loads(holmes_result.analysis)
             healthy = analysis.get("workload_healthy")
         except Exception:
-            healthy = False
+            logging.exception("Error in holmes response format, analysis did not return valid json format.")
             pass
 
         if params.silent_healthy and healthy:
