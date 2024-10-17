5be04cc383788b84a5707194ffd99bdc2aab71c8
Nicolas Herment <nherment@arkhaios.net>
2024-10-17 14:38:03 +0200

    feat: dynamic selectors with alert_foreign_logs_enricher

---
+++ playbooks/robusta_playbooks/util.py
@@ -0,0 +1,20 @@
+
+import re
+from typing import Dict, List
+
+REGEXP_FIND_SELECTOR = r'\{\{\s*labels\.([^\s]+)\s*\}\}'
+
+def resolve_selectors(labels:Dict, selectors:List[str]) -> List[str]:
+    """
+    Allows referencing of labels inside selectors and returns parsed selectors.
+
+    e.g.
+    labels={"my_label": "my_value"}
+    resolve_selectors(labels, ["{{labels.my_label}}"]) => ["my_value"]
+    """
+    def replace_label(match):
+        key = match.group(1).strip()
+        return labels.get(key, '')
+
+    resolved_selectors = [re.sub(REGEXP_FIND_SELECTOR, replace_label, selector) for selector in selectors]
+    return resolved_selectors
---
+++ tests/playbooks/test_prometheus_alert_selector.py
@@ -0,0 +1,35 @@
+
+import pytest
+from playbooks.robusta_playbooks.util import resolve_selectors
+
+@pytest.mark.parametrize(
+    "labels,input_selectors,expected_resolved_selectors",
+    [
+        ({}, ["my_selector"], ["my_selector"]),
+        ({"field1": "value1"}, ["{{ labels.field1 }}"], ["value1"]),
+        ({}, ["{{ labels.field1 }}"], [""]),
+        ({"field_1": "value1"}, ["{{ labels.field_1 }}"], ["value1"]),
+        ({"field-1": "value1"}, ["{{ labels.field-1 }}"], ["value1"]),
+        (
+            {"domain": "localhost", "port": "3000", "apiUrl": "/my_api"},
+            ["http://{{ labels.domain}}:{{ labels.port }}{{labels.apiUrl}}"],
+            ["http://localhost:3000/my_api"]),
+        (
+            {"domain": "localhost", "port": "3000", "apiUrl": "/my_api"},
+            [
+                "http://{{ labels.domain}}:{{ labels.port }}{{labels.apiUrl}}",
+                "{{ labels.domain}}",
+                "{labels.domain}",
+                "{apiUrl"
+            ],
+            [
+                "http://localhost:3000/my_api",
+                "localhost",
+                "{labels.domain}",
+                "{apiUrl"
+            ]
+        )
+    ],
+)
+def test_clean_up_k8s_logs_from_job_output(labels, input_selectors, expected_resolved_selectors):
+    assert resolve_selectors(labels, input_selectors) == expected_resolved_selectors
