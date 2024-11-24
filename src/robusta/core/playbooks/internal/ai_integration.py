d4576ae0eb9f092eba55ff7d25139ab527973ecd
avi@robusta.dev <avi@robusta.dev>
2024-11-24 09:57:29 +0200

    rebase fix

--- src/robusta/core/playbooks/internal/ai_integration.py
+++ src/robusta/core/playbooks/internal/ai_integration.py
@@ -162,8 +162,8 @@ def holmes_workload_health(event: ExecutionBaseEvent, params: HolmesWorkloadHeal
 
 
 def build_conversation_title(params: HolmesConversationParams):
-        label_string = f"with alert labels {params.labels}" if params.labels else ""
-        return f"{params.resource}, {params.ask} for issue '{params.context.robusta_issue_id}' with labels {label_string}"
+    label_string = f"with alert labels {params.labels}" if params.labels else ""
+    return f"{params.resource}, {params.ask} for issue '{params.context.robusta_issue_id}' with labels {label_string}"
 
 
 # old version of holmes conversation API
