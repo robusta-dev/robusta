ca591ea54f107e305b9d16839e45566b576cd6b2
Arik Alon <alon.arik@gmail.com>
2025-08-28 21:43:31 +0300

    Patch empty on_pod_conditions, to avoid Hikaru schema validation failure

--- src/robusta/patch/patch.py
+++ src/robusta/patch/patch.py
@@ -51,12 +51,22 @@ def patch_on_pod_conditions():
     # This fixes https://github.com/kubernetes-client/python/issues/2056 before the
     # k8s people take care of it (it's urgent for us).
 
-    logging.debug("Creating kubernetes PodFailurePolicyRUle.on_pod_conditions monkey patch")
+    logging.debug("Creating kubernetes PodFailurePolicyRule.on_pod_conditions monkey patch")
 
     def patched_setter(self, on_pod_conditions):
         self._on_pod_conditions = on_pod_conditions
 
     V1PodFailurePolicyRule.on_pod_conditions = V1PodFailurePolicyRule.on_pod_conditions.setter(patched_setter)
+    
+    original_init = V1PodFailurePolicyRule.__init__
+    
+    def patched_init(self, action=None, on_exit_codes=None, on_pod_conditions=None, local_vars_configuration=None):
+        # Provide empty list default to avoid Hikaru "None" validation
+        if on_pod_conditions is None:
+            on_pod_conditions = []
+        original_init(self, action=action, on_exit_codes=on_exit_codes, on_pod_conditions=on_pod_conditions, local_vars_configuration=local_vars_configuration)
+    
+    V1PodFailurePolicyRule.__init__ = patched_init
 
 
 def event_time(self, event_time):
