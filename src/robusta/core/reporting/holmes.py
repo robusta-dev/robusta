ee69ba76b2ee7d3b25c0870f444618843112b094
mershal <shahal@robusta.dev>
2025-09-28 18:14:18 +0300

    Allow passing custom_system_prompt param to holmes_chat action

--- src/robusta/core/reporting/holmes.py
+++ src/robusta/core/reporting/holmes.py
@@ -39,6 +39,7 @@ class HolmesChatRequest(BaseModel):
     conversation_history: Optional[List[dict]] = None
     model: Optional[str] = None
     stream: bool = Field(default=False)
+    custom_system_prompt: Optional[str] = None
 
 
 class HolmesIssueChatRequest(HolmesChatRequest):
